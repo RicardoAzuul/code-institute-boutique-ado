697b8a99e26cdfeef7623bf0e6c1c6088c6ca71b
Richard Blaauw <r.t.blaauw@live.com>
2022-03-25 20:28:07 +0000

    Complete home page header and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -147,6 +147,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # should be a tuple
+
+MEDIA_URL = '/media/'  # where all media files can be found
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # the dir where all media can be found 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
