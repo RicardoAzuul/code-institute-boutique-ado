be5bd147f3120ec547995515f2056b5bd86d5213
Richard Blaauw <r.t.blaauw@live.com>
2022-04-14 17:47:51 +0000

    Fix typo in allowed_hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ricardoazuul-boutique-ado.heroku.app.com', 'localhost']
+ALLOWED_HOSTS = ['ricardoazuul-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
